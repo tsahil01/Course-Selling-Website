4da95aa39b0b74de507787a4085e03833751868f
tsahil01 <sahiltiwaskar.21@stvincentngp.edu.in>
2024-01-30 23:18:59 +0530

    Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,14 @@
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Home from './component/Homepage'
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element = {<Home/>}></Route>
+        <Route path='/' element = {<Home/>}></Route>
+      </Routes>
+    </BrowserRouter>
+    
   )
 }
