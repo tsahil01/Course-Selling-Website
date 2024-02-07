06950841048061dedc4a539f299bb798442526d7
tsahil01 <sahiltiwaskar.21@stvincentngp.edu.in>
2024-02-07 22:28:03 +0530

    Update homepage and navbar styles

--- frontend/src/component/Homepage.jsx
+++ frontend/src/component/Homepage.jsx
@@ -9,7 +9,7 @@ export default function Home(){
     <div className='md:max-w-screen-2xl md:pt-36 pt-20 sm:pb-20 md:pb-10 mx-auto px-4 sm:px-12 text-white'>
         <div className='flex flex-col items-center justify-center'>
             <h1 className="font-bold md:text-5xl text-3xl text-center">
-            The Prime Spot for Acquiring Coding Skills!
+            The Prime Spot for Acquiring <span className="text-blue-600">Coding Skills!</span>
             </h1>
             <div className='text-sm md:text-xl text-zinc-400 text-center max-w-sm md:max-w-2xl mt-4 md:px-4 px-2'>
             An Entry-Level Haven for Excelling in Coding Proficiency and Unlocking Your Inner Programmer Brilliance! Embark on Your Learning Journey Now and Evolve into a Tech Maestro of the Future!
