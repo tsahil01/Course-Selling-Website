0ad73290793211ebb7f8409067bf724d6e9dff7e
tsahil01 <sahiltiwaskar.21@stvincentngp.edu.in>
2024-02-18 22:44:07 +0530

    Add body-parser and cors dependencies, and update index.ts to use them

--- backend-serverless/src/index.ts
+++ backend-serverless/src/index.ts
@@ -1,5 +1,11 @@
-import { Hono } from 'hono'
-const app = new Hono()
+import { Hono } from 'hono';
+import cors from 'cors';
+import { bodyParser } from "body-parser";
+
+const app = new Hono();
+
+app.use(bodyParser.json());
+app.use(cors());
 
 app.get('/', (c) => c.text('Hono!'))
 
